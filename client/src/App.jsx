a810ed5cb9e6a6c21bbf0f4e0a6661ee9560e16d
Nika <nikadvalishvili517@gmail.com>
2025-11-15 03:51:28 +0400

    Added global padding style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div className="app-layout">
+    <div className="app-layout container">
       <Header />
 
       <main className="app-content">
