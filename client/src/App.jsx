30910ef4988338a0fd8d8bf72528aa20e80e8677
Nika <nikadvalishvili517@gmail.com>
2025-11-15 23:46:34 +0400

    Made footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import ScrollIndicator from "./components/ScrollProgress/ScrollIndicator";
-
+import Footer from "./components/Footer/Footer";
 export default function App() {
   return (
     <div className="app-layout container">
@@ -11,6 +11,7 @@ export default function App() {
       <main className="app-content">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
