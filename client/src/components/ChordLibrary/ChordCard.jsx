1f33f58c7dbe4560e3f31c2dd28e00514dc72926
Nika <nikadvalishvili517@gmail.com>
2025-11-16 03:10:55 +0400

    Added difficulty logic on cards and difficulty endpoint

--- client/src/components/ChordLibrary/ChordCard.jsx
+++ client/src/components/ChordLibrary/ChordCard.jsx
@@ -1,15 +1,36 @@
 import "./ChordCard.css";
 
 export default function ChordCard({ chord }) {
+  const getDifficultyColor = (level) => {
+    switch (level.toLowerCase()) {
+      case "Beginner":
+        return "#39ff88";
+      case "intermediate":
+        return "#ff9f40";
+      case "advanced":
+        return "#ff5757";
+      default:
+        return "#39ff88";
+    }
+  };
   return (
     <div className="chord-card">
       <h3>
-        {chord.name.split(" ")[0]}{" "}
-        <span>{chord.name.split(" ")[1]}</span>
+        {chord.name.split(" ")[0]} <span>{chord.name.split(" ")[1]}</span>
       </h3>
 
       <p>Notes: {chord.notes.join(", ")}</p>
       <p>Fingers: {chord.fingers.join("")}</p>
+
+      <p>
+        Difficulty:{" "}
+        <span
+          className="diff-color"
+          style={{ color: getDifficultyColor(chord.difficulty) }}
+        >
+          {chord.difficulty}
+        </span>
+      </p>
 
       <button>VIEW CHORD</button>
     </div>
