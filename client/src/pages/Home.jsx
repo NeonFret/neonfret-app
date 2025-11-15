045b1eef15176f2943d8c7a8a2cffa28be868336
Nika <nikadvalishvili517@gmail.com>
2025-11-15 23:01:01 +0400

    Made Why NeonFret section and added responsive layout of hero and this sections

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import HomeHero from "../components/Home/HomeHero";
+import ReasonSection from "../components/Home/ReasonSection";
 
 export default function Home() {
   return (
     <>
       <HomeHero />
+      <ReasonSection />
     </>
   );
 }
